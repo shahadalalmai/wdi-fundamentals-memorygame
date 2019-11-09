f07dc82542f4e84d35ad0a96d784e4b91b64bbf2
ShahadAlalmai <honeytheshine@ShahadAlalmais-MacBook-Air.local>
2019-11-09 16:25:32 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,46 @@
-console.log("Up and running!");
+//console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+// var cardOne = "queen";
+// var cardTwo = "queen";
+// var cardThree = "king";
+// var cardFour = "king";
 
-console.log("User flipped " + cardTwo);
+ //console.log("User flipped queen");
+
+const cards = ["queen", "queen", "king", "king"];
+
+const cardsInPlay = [];
+
+function checkForMatch() {
+
+if (cardsInPlay.length === 2){
+
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match!");
+
+	} else {
+		console.log("Sorry, try again");
+	}
+}
+
+};// end of checkForMatch func
+
+function flipCard(cardId){
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+
+	checkForMatch();
+
+// var cardOne = cards[0];
+// cardsInPlay.push(cardOne);
+
+// var cardTwo = cards[2];
+// cardsInPlay.push(cardTwo);
+
+
+
+}; //end of flipCard func
+
+flipCard(0);
+flipCard(2);
+
